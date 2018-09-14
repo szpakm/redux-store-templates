c6ced4a48bef80a58a723520be71d3e23d4357be
Mateusz Szpak <mateusz.szpak@esatto.se>
2018-09-14 16:39:37 +0200

    style: cleanups

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-import * as taskSimple from './task-simple';
+import * as counter from "./counter";
+import * as list from "./list";
+import * as setSimple from "./set-simple";
+import * as taskSimple from "./task-simple";
+import * as toggle from "./toggle";
+import * as value from "./value";
 
-export { taskSimple };
+export { counter, list, setSimple, taskSimple, toggle, value };
