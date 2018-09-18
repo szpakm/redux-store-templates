a2969ac052848fcfdd450bb40039acd6b1dc4553
Mateusz Szpak <mateusz.szpak@esatto.se>
2018-09-19 00:22:20 +0200

    test(helpers): tests added

--- test/.internals/helpers.spec.js
+++ test/.internals/helpers.spec.js
@@ -1,55 +1,125 @@
 import {
   makeActionCreator,
-  createPathReader
-} from '../../src/.internals/helpers';
+  createPathReader,
+  readAsArray,
+  pick,
+  warn
+} from "../../src/.internals/helpers";
 
-describe('.internals/helpers/makeActionCrator', () => {
-  it('makeActionCrator with string parameter', () => {
-    const expectedAction = {
-      type: 'TYPE_TEST',
-      payload: 'VALUE'
-    };
-    const simpleActionCreator = makeActionCreator('TYPE_TEST');
-
-    expect(simpleActionCreator('VALUE')).toEqual(expectedAction);
+describe(".internals/helpers/readAsArray", () => {
+  it("reads string as array with string", () => {
+    expect(readAsArray("VALUE")).toEqual(["VALUE"]);
   });
 
-  it('makeActionCrator with object parameter', () => {
-    const expectedAction = {
-      type: 'TYPE_TEST',
-      payload: 'VALUE'
-    };
-    const simpleActionCreator = makeActionCreator({ type: 'TYPE_TEST' });
-
-    expect(simpleActionCreator('VALUE')).toEqual(expectedAction);
+  it("reads array without changes", () => {
+    expect(readAsArray(["VALUE"])).toEqual(["VALUE"]);
   });
 });
 
-describe('.internals/helpers/createPathReader', () => {
-  it('createPathReader for level 3', () => {
-    const data = { x: { y: { z: 'goal!' } } };
-    const read = createPathReader('x.y.z');
-
-    expect(read(data)).toEqual('goal!');
+describe(".internals/helpers/pick", () => {
+  it("returns direct field value if string parameter", () => {
+    expect(pick({ x: "X", y: "Y" }, "x")).toEqual("X");
   });
 
-  it('createPathReader for non existing field in the middle', () => {
-    const data = { x: { y: { z: 'goal!' } } };
-    const read = createPathReader('x.p.z');
+  it("returns object with field value if array parameter", () => {
+    expect(pick({ x: "X", y: "Y", z: "Z" }, ["x", "y"])).toEqual({
+      x: "X",
+      y: "Y"
+    });
+  });
+
+  it("returns whole object if no parameter", () => {
+    expect(pick({ x: "X", y: "Y" })).toEqual({ x: "X", y: "Y" });
+  });
+});
+
+describe(".internals/helpers/makeActionCrator", () => {
+  it("creates action from string parameter", () => {
+    const expectedAction = {
+      type: "TYPE_TEST",
+      payload: "VALUE"
+    };
+    const simpleActionCreator = makeActionCreator("TYPE_TEST");
+
+    expect(simpleActionCreator("VALUE")).toEqual(expectedAction);
+  });
+
+  it("creates action from object { type: string } parameter", () => {
+    const expectedAction = {
+      type: "TYPE_TEST",
+      payload: "VALUE"
+    };
+    const simpleActionCreator = makeActionCreator({ type: "TYPE_TEST" });
+
+    expect(simpleActionCreator("VALUE")).toEqual(expectedAction);
+  });
+});
+
+describe(".internals/helpers/createPathReader", () => {
+  it("reads from 3 levels deep path", () => {
+    const data = { x: { y: { z: "goal!" } } };
+    const read = createPathReader("x.y.z");
+
+    expect(read(data)).toEqual("goal!");
+  });
+
+  it("returns undefined for non existing field in the middle of path", () => {
+    const data = { x: { y: { z: "goal!" } } };
+    const read = createPathReader("x.p.z");
 
     expect(read(data)).toBeUndefined();
   });
 
-  it('createPathReader for non existing field at the beginning', () => {
+  it("returns undefined for non existing field at the beginning of path", () => {
     const data = {};
-    const read = createPathReader('x.y.z');
+    const read = createPathReader("x.y.z");
     expect(read(data)).toBeUndefined();
   });
 
-  it('createPathReader for non existing field at the end', () => {
-    const data = { x: { y: { z: 'goal!' } } };
-    const read = createPathReader('x.y.p');
+  it("returns undefined for non existing field at the end of path", () => {
+    const data = { x: { y: { z: "goal!" } } };
+    const read = createPathReader("x.y.p");
 
     expect(read(data)).toBeUndefined();
+  });
+
+  it('returns "payload" for no path provided', () => {
+    const data = { payload: "goal!" };
+    const read = createPathReader();
+
+    expect(read(data)).toEqual("goal!");
+  });
+
+  it("works if path is an Array", () => {
+    const data = { x: { y: { z: "goal!" } } };
+    const read = createPathReader(["x", "y", "z"]);
+
+    expect(read(data)).toEqual("goal!");
+  });
+});
+
+describe(".internals/helpers/warn", () => {
+  it("calls console.warn with prefix when available", () => {
+    const preSpy = console.warn;
+    const spy = jest.fn();
+
+    console.warn = spy;
+    try {
+      warn("Test");
+      expect(spy.mock.calls[0][0]).toBe("redux-store-templates: Test");
+    } finally {
+      spy.mockClear();
+      console.warn = preSpy;
+    }
+  });
+
+  it("does not throw when console.warn is not available", () => {
+    const realConsole = global.console;
+    Object.defineProperty(global, "console", { value: {} });
+    try {
+      expect(() => warn("Test")).not.toThrow();
+    } finally {
+      Object.defineProperty(global, "console", { value: realConsole });
+    }
   });
 });
